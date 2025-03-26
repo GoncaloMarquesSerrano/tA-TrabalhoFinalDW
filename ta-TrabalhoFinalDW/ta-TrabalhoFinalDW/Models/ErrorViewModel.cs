1874fc0e6022b953e6e7951b6198f5f418da1a07
Gonçalo Serrano <aluno25948@ipt.pt>
2025-03-26 18:17:27 +0000

    Commit inicial

---
+++ ta-TrabalhoFinalDW/ta-TrabalhoFinalDW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ta_TrabalhoFinalDW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
